eb0f6a4f8951ba61669eec8a355480330ffac973
robin <tjzhuorui@163.com>
2017-04-10 18:23:02 +0800

    1

--- MyBatis_Training/src/com/robin/pojo/student.java
+++ MyBatis_Training/src/com/robin/pojo/student.java
@@ -1,5 +1,40 @@
 package com.robin.pojo;
 
 public class student {
-	
+	private int std_id;
+	private String name;
+	private String birthday;
+	private Enum Sex;
+
+	public int getStd_id() {
+		return std_id;
+	}
+
+	public void setStd_id(int std_id) {
+		this.std_id = std_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public Enum getSex() {
+		return Sex;
+	}
+
+	public void setSex(Enum sex) {
+		Sex = sex;
+	}
 }
